bf2add27e0951919634ba8d9c43caf193c523dd0
oborysewicz <oborysewicz@gmail.com>
2021-10-28 20:59:27 +0000

    initial commit - terraform

---
+++ terraform/rmq/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+    region = "eu-west-3"
+    profile = var.profile
+}
+
+resource "aws_instance" "rmq" {
+    ami = "ami-07ebfd5b3428b6f4d"
+    instance_type = "t2.micro"
+    key_name = "rabbitmq"
+    vpc_security_group_ids = ["sg-0d037a4ed51734e81"]
+
+    tags = {
+        Name = var.name
+        group = var.group
+    }
+}
